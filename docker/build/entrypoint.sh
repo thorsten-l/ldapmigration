5685c4a4b4d4252f45e97c8941c51c4c5844b141
Thorsten Ludewig <th@l9g.de>
2022-11-08 13:09:39 +0100

    entrypoint fixed

--- docker/build/entrypoint.sh
+++ docker/build/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ ! -d "/data/etc" ]; then
-  echo "Initialize /data/etc" 
+if [ ! -d "/data/config" ]; then
+  echo "Initialize /data" 
   cp -r /etc/dirsrv.dist/* /data
 fi
 exec /usr/libexec/dirsrv/dscontainer -r
